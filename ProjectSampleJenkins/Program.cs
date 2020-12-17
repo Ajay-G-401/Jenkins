c97f86c730cae9058e311abdbf3df9d2c713b34d
Ajay-G-401 <76032583+Ajay-G-401@users.noreply.github.com>
2020-12-17 13:14:19 +0530

    Update Program.cs
    error

--- ProjectSampleJenkins/Program.cs
+++ ProjectSampleJenkins/Program.cs
@@ -11,6 +11,7 @@ namespace ProjectSampleJenkins
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+             Console.WriteLine("checking build creating error");
         }
     }
 }
